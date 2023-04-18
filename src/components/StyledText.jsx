92c59713ce4299e7a273d804723d40487775cbe2
rodolfo-pedroza <rudy_pedroza@hotmail.com>
2023-04-17 19:34:11 -0600

    component to style text usign theme as a guide

---
+++ src/components/StyledText.jsx
@@ -0,0 +1,44 @@
+import { Text, StyleSheet } from "react-native";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+    text: {
+        color: theme.colors.black,
+        fontSize: theme.fontSizes.body,
+        fontFamily: theme.fonts.main,
+        fontWeight: theme.fontWeights.normal,
+    },
+    colorPrimary: {
+        color: theme.colors.blueShadow,
+    },
+    colorSecondary: {
+        color: theme.colors.purpleShadow,
+    },
+    bold: {
+        fontWeight: theme.fontWeights.bold,
+    },
+    fontSizeSubheading: {
+        fontSize: theme.fontSizes.subheading,
+    },
+    textAlign: {
+        textAlign: "center",
+    },
+});
+
+export default function StyledText({ children, color, fontSize, fontWeight, align, style, ...restOfProps }) {
+    const textStyles = [
+        styles.text,
+        color === "primary" && styles.colorPrimary,
+        color === "secondary" && styles.colorSecondary,
+        fontSize === "subheading" && styles.fontSizeSubheading,
+        fontWeight === "bold" && styles.bold,
+        align === "center" && styles.textAlign,
+        style,
+    ];
+
+    return (
+        <Text style={textStyles} {...restOfProps}>
+            {children}
+        </Text>
+    );  
+}

cca1328b0339f40a6a3be15f7271060140801345
rodolfo-pedroza <rudy_pedroza@hotmail.com>
2023-04-17 19:31:29 -0600

    file with basic css styles added as a guide

---
+++ src/theme.js
@@ -0,0 +1,36 @@
+const theme = {
+  appBar: {
+    primary: "#24292e",
+    textSecondary: "#888",
+    textPrimary: "#fff",
+  },
+  colors: {
+    gray1: "#7B6F72",
+    gray2: "#ADA4A5",
+    gray3: "#DDDADA",
+    white: "#FFFFFF",
+    black: "#1D1617",
+    borderColor: "#F7F8F8",
+    success: "#42D742",
+    warning: "#FFD600",
+    danger: "#FF0000",
+  },
+  effects: {
+    cardShadow: "0px 10px 40px rgba(29, 22, 23, 0.07)",
+    blueShadow: "0px 10px 22px rgba(149, 173, 254, 0.3)",
+    purpleShadow: "0px 10px 22px rgba(197, 139, 242, 0.3)",
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+  },
+  fonts: {
+    main: "System",
+  },
+  fontWeights: {
+    normal: "400",
+    bold: "700",
+  },
+};
+
+export default theme;

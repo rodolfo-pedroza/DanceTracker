dbdc284613fdb704a4e54ac8a51d6c0314a8ba7f
rodolfo-pedroza <rudy_pedroza@hotmail.com>
2023-04-17 19:34:37 -0600

    basic components for the routing system

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome to the Home page!</Text>
+    </View>
+  );
+}
+
+export default Home;

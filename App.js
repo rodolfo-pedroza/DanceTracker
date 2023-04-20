8bf326c33ac7fabb99e07caaaa98b62c366039d8
rodolfo-pedroza <rudy_pedroza@hotmail.com>
2023-04-19 18:11:02 -0600

    consditional rendering added

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import Main from "./src/components/Main";
 import { NativeRouter } from "react-router-native";
-import Login from "./src/pages/Login";
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Constants from "expo-constants";
+import AutenticatedRoutes from "./src/components/AutenticatedRoutes";
+import UnauthenticatedRoutes from "./src/components/UnauthenticatedRoutes";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+  },
+});
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
       <StatusBar style="auto" />
       <NativeRouter>
-        <Main />
+        <View style={styles.container}>
+          {user ? (
+            <AutenticatedRoutes />
+          ) : (
+            <UnauthenticatedRoutes setUser={setUser} />
+          )}
+        </View>
       </NativeRouter>
     </>
   );

5f85a228a48fc63b2bfb0a294d66daedc923147a
rodolfo-pedroza <rudy_pedroza@hotmail.com>
2023-04-19 18:15:58 -0600

    basic home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 function Home() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Welcome to the Home page!</Text>
+      <Text>Working on home page</Text>
     </View>
   );
 }
